4d96526e4c0e32ee0bd453794a0dbbe5a65b341e
albert-881 <quinteroalberto88@gmail.com>
2024-09-12 14:04:57 -0500

    added a better message when submitting a quote request and after i submit a request the input fields clear

--- script.js
+++ script.js
@@ -7,11 +7,45 @@ document.getElementById('quote-form').addEventListener('submit', function(event)
   
     // Send form data to EmailJS
     emailjs.sendForm('service_cfz2lkq', 'template_w6jqiy4', this)
-      .then(function(response) {
-        console.log('SUCCESS!', response.status, response.text);
-        alert('Quote request successfully sent!');
-      }, function(error) {
-        console.log('FAILED...', error);
-        alert('Failed to send quote request. Please try again.');
+      .then(function() {
+        const successMsg = document.querySelector('#message-status');
+        successMsg.textContent = 'Quote request successfully sent!';
+        clear();
+      }, function() {
+        const errorMsg = document.querySelector('#message-status');
+        errorMsg.textContent = 'Failed to send quote request. Please try again.';
       });
 });
+
+
+const navigator = document.querySelector('.navi-buttons');
+navigator.addEventListener('click', (e) => {
+    let buttonid = e.target.id;
+    
+    switch(buttonid){
+      case 'service':
+        const serviceSection = document.querySelector('.services-section');
+        serviceSection.scrollIntoView({behavior:'smooth'});
+        break;
+
+      case 'quote':
+        const quoteSection = document.querySelector('.quote-section');
+        quoteSection.scrollIntoView({behavior:'smooth'});
+        break;
+    } 
+    
+});
+
+
+
+
+function clear() {
+  const clearInputs = document.querySelectorAll('input');
+  clearInputs.forEach(input => input.value = '');
+
+  const clearSelect = document.querySelector('select');
+  clearSelect.value = 'trailer-drop-off';
+
+  const clearText = document.querySelector('textarea');
+  clearText.value = '';
+}
